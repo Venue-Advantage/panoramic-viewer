4fe5af521edbf7e3f51085f990fe0c05f7b9ab4f
Sandeep Singh <sqmlaptop-45@Sandeeps-MacBook-Pro.local>
2025-03-25 09:07:19 -0400

    6 image view added

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,26 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import KrpanoImg from './assets/krpano-img.png';
+import Front from './assets/6-set-img-krpano/front.png';
+import Back from './assets/6-set-img-krpano/back.png';
+import Left from './assets/6-set-img-krpano/left.png';
+import Right from './assets/6-set-img-krpano/right.png';
+import Top from './assets/6-set-img-krpano/top.png';
+import Bottom from './assets/6-set-img-krpano/bottom.png';
 import './App.css'
 import PanoramaViewer from './components/PanoramaViewer';
 
 function App() {
-  const [view, setView] = useState("sphere")
+  const [view, setView] = useState("cube")
   const [imageFile, setImageFile] = useState(KrpanoImg);
-
+  const cubeImages = [
+    Front,
+    Back,
+    Top,
+    Bottom,
+    Left,
+    Right
+  ];
   return (
     <>
       {/* <div>
@@ -37,7 +50,7 @@ function App() {
       </div> */}
 
       <div style={{ width: "100vw", height: "100vh" }}>
-        <PanoramaViewer imageUrl={imageFile} view={view} />
+        <PanoramaViewer imageUrl={imageFile} view={view} imageSet={cubeImages} />
       </div>
     </>
   )
--- src/components/PanoramaViewer.tsx
+++ src/components/PanoramaViewer.tsx
@@ -2,23 +2,49 @@ import React from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Sphere, Box } from "@react-three/drei";
 import * as THREE from "three";
+import { useMemo } from "react";
+// import Front from '../assets/6-set-img-krpano/front.png';
 
-const PanoramaViewer = ({ imageUrl, view }) => {
+
+const PanoramaViewer = ({ imageUrl, view, imageSet }) => {
     const texture: any = new THREE.TextureLoader().load(imageUrl);
     // console.log("texture", texture);
-    if (view === "sphere") {
+    const materials = useMemo(
+        () =>
+            imageSet.map(
+                (img: any) =>
+                    new THREE.MeshBasicMaterial({
+                        map: new THREE.TextureLoader().load(img),
+                        side: THREE.BackSide, // Render textures inside the cube
+                    })
+            ),
+        [imageSet]
+    );
+
+    if (view === "cube") {
         return (
-            <Canvas camera={{ position: [0, 0, 2], fov: 20 }}>
-                <ambientLight intensity={0.5} />
-                <directionalLight position={[10, 10, 10]} />
+            <Canvas camera={{ position: [0, 0, 0], fov: 90 }}>
+                <mesh>
+                    <boxGeometry args={[10, 10, 10]} /> {/* Larger cube so we are inside */}
+                    {materials.map((material: any, index: any) => (
+                        <meshBasicMaterial key={index} attach={`material-${index}`} {...material} />
+                    ))}
+                </mesh>
 
-                {/* Sphere with Inverted Normals (to show image from inside) */}
-                <Sphere args={[5, 60, 40]} scale={[-1, 1, 1]}>
-                    <meshBasicMaterial map={texture} side={THREE.BackSide} />
-                </Sphere>
+                {/* <Canvas camera={{ position: [0, 0, 8] }}>
+                    <ambientLight intensity={0.5} />
+                    <directionalLight position={[5, 5, 5]} />
+                    <mesh>
+                        <boxGeometry args={[5, 5, 5]} />
+                        {materials.map((material, index) => (
+                            <meshBasicMaterial key={index} attach={`material-${index}`} {...material} />
+                        ))}
+                    </mesh>
+                    <OrbitControls enableZoom={true} zoomSpeed={0.8} minDistance={2} maxDistance={10} />
+                </Canvas> */}
 
-                {/* Allows user to rotate the scene */}
                 <OrbitControls
+
                     enableZoom={true}
                     zoomSpeed={0.5}   // Faster zooming
                     minDistance={0} // Ultra close zoom-in
@@ -27,28 +53,55 @@ const PanoramaViewer = ({ imageUrl, view }) => {
                     maxPolarAngle={Math.PI - 0.1}
                     minPolarAngle={0.1}
                 />
-
-                  {/* <OrbitControls  enableZoom={false} /> */}
-            </Canvas>
-        );
-    } else if (view === "cube") {
-        return (
-            <Canvas camera={{ position: [2, 2, 2] }}>
-                <ambientLight intensity={0.5} />
-                <directionalLight position={[10, 10, 10]} />
-
-                {/* Cube with the same image on all six sides */}
-                <Box args={[2, 2, 2]}>
-                    {[...Array(6)].map((_, index) => (
-                        <meshBasicMaterial key={index} attach={`material-${index}`} map={texture} />
-                    ))}
-                </Box>
-
-                {/* Enables dragging to look around */}
-                <OrbitControls />
             </Canvas>
         );
     }
+
+
+    // OLD CODE
+    // if (view === "sphere") {
+    //     return (
+    //         <Canvas camera={{ position: [0, 0, 2], fov: 20 }}>
+    //             <ambientLight intensity={0.5} />
+    //             <directionalLight position={[10, 10, 10]} />
+
+    //             {/* Sphere with Inverted Normals (to show image from inside) */}
+    //             <Sphere args={[5, 60, 40]} scale={[-1, 1, 1]}>
+    //                 <meshBasicMaterial map={texture} side={THREE.BackSide} />
+    //             </Sphere>
+
+    //             {/* Allows user to rotate the scene */}
+    //             <OrbitControls
+    //                 enableZoom={true}
+    //                 zoomSpeed={0.5}   // Faster zooming
+    //                 minDistance={0} // Ultra close zoom-in
+    //                 maxDistance={15}      // Prevents zooming too far out
+    //                 enablePan={false}
+    //                 maxPolarAngle={Math.PI - 0.1}
+    //                 minPolarAngle={0.1}
+    //             />
+
+    //               {/* <OrbitControls  enableZoom={false} /> */}
+    //         </Canvas>
+    //     );
+    // } else if (view === "cube") {
+    //     return (
+    //         <Canvas camera={{ position: [2, 2, 2] }}>
+    //             <ambientLight intensity={0.5} />
+    //             <directionalLight position={[10, 10, 10]} />
+
+    //             {/* Cube with the same image on all six sides */}
+    //             <Box args={[2, 2, 2]}>
+    //                 {[...Array(6)].map((_, index) => (
+    //                     <meshBasicMaterial key={index} attach={`material-${index}`} map={texture} />
+    //                 ))}
+    //             </Box>
+
+    //             {/* Enables dragging to look around */}
+    //             <OrbitControls />
+    //         </Canvas>
+    //     );
+    // }
 };
 
 export default PanoramaViewer;
