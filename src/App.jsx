def4db41ec3efb16911980a9486e97473284f925
Sandeep Singh <sqmlaptop-45@Sandeeps-MacBook-Pro.local>
2025-04-23 09:59:42 +0530

    cloudinary/image-kit image url set with template xml

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,12 @@ import './App.css'
 const App = () => {
   const krpanoRef = useRef(null);
   const [krpanoLoaded, setKrpanoLoaded] = useState(false);
- 
+  const CDN_ACCOUNT_ID = "lzcu8n7b2"; // Set by imagekit/Cloudinary or any other CDN
+  const IMAGE_BASE_URL = `https://ik.imagekit.io/${CDN_ACCOUNT_ID}/kr-pano`; // Set by imagekit/Cloudinary or any other CDN
+
   useEffect(() => {
     const scriptId = "krpano-script";
-  
+
     if (!document.getElementById(scriptId)) {
       const script = document.createElement("script");
       script.id = scriptId;
@@ -24,26 +26,49 @@ const App = () => {
     }
   }, []);
 
-  useEffect(() => {
+  // useEffect(() => { // old way with static XML and static images
+  //   if (!krpanoLoaded || !window.embedpano) return;
+
+  //   window.embedpano({
+  //     target: krpanoRef.current,
+  //     xml: "/panoramic-viewer/pano.xml", // XML now correctly loads from public folder
+  //     html5: "only",
+  //     // passQueryParameters: false,
+  //     onready: (krpano) => {
+  //       console.log("krpano is ready", krpano);
+  //       setTimeout(() => {
+  //         // krpano.call("loadscene(myScene, null, MERGE);");
+  //         krpano.actions.loadscene("myScene");
+  //       }, 500);
+  //     },
+  //   });
+  // }, [krpanoLoaded]);
+
+
+  useEffect(() => { // dynamic image url way to load XML with fetch so that we can use a template with {{IMG_BASE}} and {{TS}} to avoid krpano to caching image 
     if (!krpanoLoaded || !window.embedpano) return;
-  
-    window.embedpano({
-      target: krpanoRef.current,
-      xml: "/panoramic-viewer/pano.xml", // XML now correctly loads from public folder
-      html5: "only",
-      // passQueryParameters: false,
-      onready: (krpano) => {
-        console.log("krpano is ready", krpano);
-        setTimeout(() => {
-          // krpano.call("loadscene(myScene, null, MERGE);");
-          krpano.actions.loadscene("myScene");
-        }, 500);
-      },
-    });
+
+    fetch('/panoramic-viewer/pano-template.xml').then(res => res.text())
+      .then(xmlTemplate => {
+        const ts = Date.now();
+        const xml = xmlTemplate
+          .replace(/{{IMG_BASE}}/g, IMAGE_BASE_URL)
+          .replace(/{{TS}}/g, ts);
+        const blob = new Blob([xml], { type: "text/xml" });
+        const xmlURL = URL.createObjectURL(blob);
+        // console.log("XML URL:", xmlURL);
+        window.embedpano({
+          target: krpanoRef.current,
+          xml: xmlURL,
+          html5: "only",
+          onready: (krpano) => {
+            console.log("krpano is ready");
+            krpano.actions.loadscene("myScene");
+          }
+        });
+      });
   }, [krpanoLoaded]);
 
-
-  // return <div ref={krpanoRef} style={{ width: "800px", height: "800px" }} />;
   return <div ref={krpanoRef} style={{ width: "100vw", height: "100vh", position: "absolute", top: 0, left: 0 }} />;
 
 };
